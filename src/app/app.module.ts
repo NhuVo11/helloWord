f582e8362e66cda2c66b485b89e355c16ed0232c
18130169-VoNguyenKhaNhu <18130169@st.hmuaf.edu.vn>
2022-04-17 10:00:26 +0700

    addFile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -11,9 +12,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserModule,FormsModule,
+    AppRoutingModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = "Như Võ";
   public age = 20;
+  public fruits = ["apple", "banana", "orange"];
   constructor() { }
-
   ngOnInit(): void {
   }
 
